5df68c6c2d04ded235194c875062f28a26386746
jackschu <js9pa@virginia.edu>
2021-08-15 16:50:35 -0400

    styling dropdown

--- src/TOFDropdown.js
+++ src/TOFDropdown.js
@@ -1,33 +1,49 @@
 // @flow strict
 
 import * as React from "react";
+import styles from "./TOFDropdown.module.css";
 
 type Props = {
   labels: $ReadOnlyArray<string>,
   selectedLabels: $ReadOnlyArray<string>,
   onSelect: ($ReadOnlyArray<string>) => void,
   multiple?: ?boolean,
+  placeholder?: ?string,
 };
+
 export default function TOFDropdown({
   labels,
   selectedLabels,
   onSelect,
   multiple,
+  placeholder,
 }: Props): React.MixedElement {
   return (
-    <select
-      multiple={multiple ?? false}
-      value={selectedLabels}
-      onChange={(event: { target: HTMLSelectElement }) => {
-        const selected = Array.from(event.target.selectedOptions);
-        onSelect(selected.map((item) => item.label));
-      }}
-    >
-      {labels.map((label) => (
-        <option key={label} value={label}>
-          {label}
-        </option>
-      ))}
-    </select>
+    <>
+      <div className={styles.dropdowninput}>
+        <div style={{ gridColumn: 1 }}>
+          {selectedLabels.length > 0
+            ? selectedLabels.join(", ")
+            : placeholder ?? "Select option(s)"}
+        </div>
+        <div style={{ gridColumn: 2 }} className={styles.triangle} />
+      </div>
+
+      <select
+        className={styles.dropdowninput}
+        multiple={multiple ?? false}
+        value={selectedLabels}
+        onChange={(event: { target: HTMLSelectElement }) => {
+          const selected = Array.from(event.target.selectedOptions);
+          onSelect(selected.map((item) => item.label));
+        }}
+      >
+        {labels.map((label) => (
+          <option key={label} value={label}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
