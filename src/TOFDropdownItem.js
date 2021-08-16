51555596984c85f5f4ce4820e85dd827f6c5118d
jackschu <js9pa@virginia.edu>
2021-08-15 20:24:05 -0400

    on hover

--- src/TOFDropdownItem.js
+++ src/TOFDropdownItem.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import styles from "./TOFDropdownItem.module.css";
 import type { Item } from "./useSelectedItems.js";
+import { useState } from "react";
 type Props = {
   item: Item,
   onSelect: (string) => void,
@@ -14,22 +15,33 @@ export default function TOFDropdown({
   onSelect,
   multiple,
 }: Props): React.MixedElement {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => setIsHovered(true);
+
+  const handleMouseLeave = () => setIsHovered(false);
+
   return (
-    <div>
-      <div
-        className={styles.container}
-        onClick={() => {
-          onSelect(item.label);
-        }}
-      >
-        <input
-          className={styles.box}
-          type={multiple ? "checkbox" : "radio"}
-          readOnly={true}
-          checked={item.isSelected}
-        ></input>
-        <div className={styles.text}>{item.label}</div>
-      </div>
+    <div
+      className={styles.container}
+      onClick={() => {
+        onSelect(item.label);
+      }}
+      style={{
+        ...(isHovered && {
+          //HACK should use a color pallette
+          backgroundColor: "#612940",
+        }),
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <input
+        className={styles.box}
+        type={multiple ? "checkbox" : "radio"}
+        readOnly={true}
+        checked={item.isSelected}
+      ></input>
+      <div className={styles.text}>{item.label}</div>
     </div>
   );
 }
